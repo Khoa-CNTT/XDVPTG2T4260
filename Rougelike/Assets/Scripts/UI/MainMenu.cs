d78d126a59d313610776c25954693ad3679ad7d6
TranHuuMinhDuc <ytientvhue@gmail.com>
2025-05-07 15:20:11 +0700

    Ui(Main Menu) & SpriteSheet

---
+++ Rougelike/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        if(UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+        
+    }
+}
